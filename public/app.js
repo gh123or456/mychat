33650be53432484cabfa900cd9f2be0ea87e9c7f
gh123or456 <gh1010801310@gmail.com>
2016-06-28 02:03:35 +0900

    node

---
+++ public/app.js
@@ -0,0 +1,14 @@
+var AppComponent = ng
+       .Component({
+           selector: 'my-app'
+       })
+       .View({
+           template: '<h1>My First Angular 2 App</h1>'
+       })
+       .Class({
+           constructor: function () { }
+       });
+
+   document.addEventListener('DOMContentLoaded', function() {
+     ng.bootstrap(AppComponent);
+   });
