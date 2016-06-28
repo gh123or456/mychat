6d4e4c4dc5643683dddfc039a1a9eb21003e2962
gh123or456 <gh1010801310@gmail.com>
2016-06-28 21:13:44 +0900

    test

--- target/scala-2.10/src_managed/main/views/html/login.template.scala
+++ target/scala-2.10/src_managed/main/views/html/login.template.scala
@@ -23,10 +23,10 @@ object login extends BaseScalaTemplate[play.api.templates.HtmlFormat.Appendable,
 Seq[Any](format.raw/*1.37*/("""
 """),format.raw/*3.1*/("""
 """),_display_(Seq[Any](/*4.2*/main("login")/*4.15*/{_display_(Seq[Any](format.raw/*4.16*/("""
+    <script src=""""),_display_(Seq[Any](/*5.19*/routes/*5.25*/.Assets.at("app.js"))),format.raw/*5.45*/(""""></script>
     <div class="login">
-
         <h1>=<0>ω<0>= < TEST !</h1>
-
+        <my-app></my-app>
         """),_display_(Seq[Any](/*9.10*/form(routes.Auth.check)/*9.33*/{_display_(Seq[Any](format.raw/*9.34*/("""
             <div class="form-group">
                 <input class="form-control input_name" id="exampleInputEmail1" type="text" name="name"  placeholder="input_name">
@@ -51,11 +51,11 @@ Seq[Any](format.raw/*1.37*/("""
 }
                 /*
                     -- GENERATED --
-                    DATE: Wed Jun 15 00:13:41 JST 2016
+                    DATE: Tue Jun 28 01:36:03 JST 2016
                     SOURCE: E:/Users/gh123or456/workspace/playframework/mychat/mychat/app/views/login.scala.html
-                    HASH: 2128e721c9e876737bc4c48764930793d8907a72
-                    MATRIX: 584->1|730->36|758->56|795->59|816->72|854->73|966->150|997->173|1035->174|1608->715
-                    LINES: 19->1|23->1|24->3|25->4|25->4|25->4|30->9|30->9|30->9|40->19
+                    HASH: 791cb8a18db7a9cef16d2fdfd60328e6aacdf5a6
+                    MATRIX: 584->1|730->36|758->56|795->59|816->72|854->73|909->93|923->99|964->119|1110->230|1141->253|1179->254|1752->795
+                    LINES: 19->1|23->1|24->3|25->4|25->4|25->4|26->5|26->5|26->5|30->9|30->9|30->9|40->19
                     -- GENERATED --
                 */
             
--- target/scala-2.10/src_managed/main/views/html/main.template.scala
+++ target/scala-2.10/src_managed/main/views/html/main.template.scala
@@ -30,9 +30,9 @@ Seq[Any](format.raw/*1.32*/("""
         <link href=""""),_display_(Seq[Any](/*9.22*/routes/*9.28*/.Assets.at("Bootstrap/css/bootstrap.min.css"))),format.raw/*9.73*/("""" rel="stylesheet">
         <link rel="stylesheet" media="screen" href=""""),_display_(Seq[Any](/*10.54*/routes/*10.60*/.Assets.at("stylesheets/main.css"))),format.raw/*10.94*/("""">
         <link rel="shortcut icon" type="image/png" href=""""),_display_(Seq[Any](/*11.59*/routes/*11.65*/.Assets.at("images/favicon.png"))),format.raw/*11.97*/("""">
-        <script src=""""),_display_(Seq[Any](/*12.23*/routes/*12.29*/.Assets.at("javascripts/jquery-1.9.0.min.js"))),format.raw/*12.74*/("""" type="text/javascript"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        <script src=""""),_display_(Seq[Any](/*14.23*/routes/*14.29*/.Assets.at("Bootstrap/js/bootstrap.min.js"))),format.raw/*14.72*/("""" type="text/javascript"></script>
+        <script src=""""),_display_(Seq[Any](/*13.23*/routes/*13.29*/.Assets.at("Bootstrap/js/bootstrap.min.js"))),format.raw/*13.72*/("""" type="text/javascript"></script>
+        <script src=""""),_display_(Seq[Any](/*14.23*/routes/*14.29*/.Assets.at("node_modules/angular2/bundles/angular2.sfx.dev.js"))),format.raw/*14.92*/("""" type="text/javascript"></script>
     </head>
     <body>
         """),_display_(Seq[Any](/*17.10*/content)),format.raw/*17.17*/("""
@@ -49,11 +49,11 @@ Seq[Any](format.raw/*1.32*/("""
 }
                 /*
                     -- GENERATED --
-                    DATE: Sun Jun 05 05:50:01 JST 2016
+                    DATE: Tue Jun 28 21:04:36 JST 2016
                     SOURCE: E:/Users/gh123or456/workspace/playframework/mychat/mychat/app/views/main.scala.html
-                    HASH: 94feb88b110dfa52b499c124a83f3ac0c3c52e3e
-                    MATRIX: 560->1|684->31|779->91|805->96|1046->302|1060->308|1126->353|1236->427|1251->433|1307->467|1405->529|1420->535|1474->567|1536->593|1551->599|1618->644|1811->801|1826->807|1891->850|1997->920|2026->927
-                    LINES: 19->1|22->1|26->5|26->5|30->9|30->9|30->9|31->10|31->10|31->10|32->11|32->11|32->11|33->12|33->12|33->12|35->14|35->14|35->14|38->17|38->17
+                    HASH: b34742ddd516c7f869dc2e6b91821b339ec50a8b
+                    MATRIX: 560->1|684->31|779->91|805->96|1046->302|1060->308|1126->353|1236->427|1251->433|1307->467|1405->529|1420->535|1474->567|1635->692|1650->698|1715->741|1809->799|1824->805|1909->868|2015->938|2044->945
+                    LINES: 19->1|22->1|26->5|26->5|30->9|30->9|30->9|31->10|31->10|31->10|32->11|32->11|32->11|34->13|34->13|34->13|35->14|35->14|35->14|38->17|38->17
                     -- GENERATED --
                 */
             
